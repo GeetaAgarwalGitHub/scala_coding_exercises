5353a4b8873a95cb1fa643193154a32cc26ef43a
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-02-11 20:39:28 +0000

    Create Person.scala
    Created Person class

---
+++ src/main/scala/com/techreturners/exercise002/Person.scala
@@ -0,0 +1,8 @@
+package com.techreturners.exercise002
+
+/**
+ * Here we are making user of the concept of a 'case' class from Scala to create an immutable
+ * object.
+ * https://docs.scala-lang.org/tour/case-classes.html
+ */
+case class Person(firstName: String, lastName: String, city: String, age: Int)
