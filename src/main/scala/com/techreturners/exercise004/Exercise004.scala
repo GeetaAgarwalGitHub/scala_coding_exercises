64258406c94fedd94fb2b28858ea9b3a9ae816ab
Vijay Agarwal <AGARWAV@domesticandgeneral.com>
2022-02-12 15:22:40 +0000

    Update Exercise004.scala
    Implemented constructor for the class

--- src/main/scala/com/techreturners/exercise004/Exercise004.scala
+++ src/main/scala/com/techreturners/exercise004/Exercise004.scala
@@ -3,12 +3,18 @@ import java.time.temporal.ChronoUnit
 import java.time.{LocalDate, LocalDateTime, Month}
 import java.time.{LocalDate, LocalDateTime}
 
+
+
 class Exercise004(var dateTime: LocalDateTime) {
 
   // Hint take a look at Scala auxiliary constructors
   // https://docs.scala-lang.org/overviews/scala-book/classes-aux-constructors.html
   // Try constructing a "LocalDateTime" from a "LocalDate"
   // You might need to default the time to be something like midnight
+
+  def this(date: LocalDate) {
+    this(date.atStartOfDay());
+  }
 
 
   def getDateTimeWithGigaSecond: LocalDateTime =
